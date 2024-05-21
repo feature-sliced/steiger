e6cfd10dc53b23c51df876c6d8a08f90b6e60452
Lev Chelyadinov <leva181777@gmail.com>
2024-05-21 23:25:47 +0200

    Implement `excessive-slicing`

---
+++ packages/fsd-rules/src/excessive-slicing/index.spec.ts
@@ -0,0 +1,118 @@
+import { expect, it } from 'vitest'
+
+import { parseIntoFsdRoot } from '../_lib/prepare-test.js'
+import excessiveSlicing from './index.js'
+
+it('reports no errors on projects with moderate slicing', () => {
+  const root = parseIntoFsdRoot(`
+    📂 entities
+      📂 users
+        📂 ui
+        📄 index.ts
+      📂 posts
+        📂 ui
+        📄 index.ts
+    📂 shared
+      📂 ui
+        📄 index.ts
+        📄 Button.tsx
+  `)
+
+  const { diagnostics } = excessiveSlicing.check(root)
+
+  expect(diagnostics).toEqual([])
+})
+
+it('reports errors on a project with an excessive amount of features', () => {
+  const root = parseIntoFsdRoot(`
+    📂 entities
+      📂 users
+        📂 ui
+        📄 index.ts
+      📂 posts
+        📂 ui
+        📄 index.ts
+    📂 shared
+      📂 ui
+        📄 index.ts
+        📄 Button.tsx
+    📂 features
+      📂 comments
+        📂 ui
+        📄 index.ts
+      📂 posts
+        📂 ui
+        📄 index.ts
+      📂 users
+        📂 ui
+        📄 index.ts
+      📂 cars
+        📂 ui
+        📄 index.ts
+      📂 alligators
+        📂 ui
+        📄 index.ts
+      📂 whales
+        📂 ui
+        📄 index.ts
+      📂 giraffes
+        📂 ui
+        📄 index.ts
+      📂 buses
+        📂 ui
+        📄 index.ts
+      📂 trains
+        📂 ui
+        📄 index.ts
+      📂 planes
+        📂 ui
+        📄 index.ts
+      📂 boats
+        📂 ui
+        📄 index.ts
+      📂 submarines
+        📂 ui
+        📄 index.ts
+      📂 helicopters
+        📂 ui
+        📄 index.ts
+      📂 rockets
+        📂 ui
+        📄 index.ts
+      📂 satellites
+        📂 ui
+        📄 index.ts
+      📂 space-stations
+        📂 ui
+        📄 index.ts
+      📂 planets
+        📂 ui
+        📄 index.ts
+      📂 galaxies
+        📂 ui
+        📄 index.ts
+      📂 universes
+        📂 ui
+        📄 index.ts
+      📂 multiverses
+        📂 ui
+        📄 index.ts
+      📂 metaverses
+        📂 ui
+        📄 index.ts
+      📂 ai
+        📂 ui
+        📄 index.ts
+      📂 bitcoin
+        📂 ui
+        📄 index.ts
+  `)
+
+  const { diagnostics } = excessiveSlicing.check(root)
+
+  expect(diagnostics).toEqual([
+    {
+      message: 'Layer "features" has 23 ungrouped slices, which is above the recommended threshold of 20. Consider grouping them or moving the code inside to the layer where it\'s used.',
+    },
+  ])
+})
---
+++ packages/fsd-rules/src/excessive-slicing/index.ts
@@ -0,0 +1,39 @@
+import { getLayers, getSlices, isSliced } from '@feature-sliced/filesystem'
+import type { Diagnostic, Rule } from '../types.js'
+import { groupSlices } from '../_lib/group-slices.js'
+
+const THRESHOLDS = {
+  entities: 20,
+  features: 20,
+  widgets: 20,
+  pages: 20,
+}
+
+/** Warn about excessive amounts of ungrouped entities/features/widgets/pages. */
+const excessiveSlicing = {
+  name: 'excessive-slicing',
+  check(root) {
+    const diagnostics: Array<Diagnostic> = []
+
+    for (const [layerName, layer] of Object.entries(getLayers(root))) {
+      if (!isSliced(layer)) {
+        continue
+      }
+
+      const sliceGroups = groupSlices(Object.keys(getSlices(layer)))
+      const threshold = THRESHOLDS[layerName as keyof typeof THRESHOLDS]
+
+      for (const [group, slices] of Object.entries(sliceGroups)) {
+        if (slices.length > threshold) {
+          diagnostics.push({
+            message: `Layer "${layerName}" has ${slices.length} ${group === '' ? 'ungrouped slices' : `slices in group "${group}"`}, which is above the recommended threshold of ${threshold}. Consider grouping them or moving the code inside to the layer where it's used.`,
+          })
+        }
+      }
+    }
+
+    return { diagnostics }
+  },
+} satisfies Rule
+
+export default excessiveSlicing
