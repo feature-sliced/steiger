fcabf3d3b355b2b06519410d4baa990b3f1d3ca0
Daniil Sapa <sapadaniil@gmail.com>
2024-08-20 19:13:36 +0300

    Implement a function that creates a copy of VFS for enabled rules

---
+++ packages/steiger/src/models/config/create-vfs-for-rules.ts
@@ -0,0 +1,19 @@
+import { flattenFolder, copyFsEntity } from '../../shared/file-system'
+import { Folder, File } from '@steiger/types'
+
+export default function createVfsForRules(rules: Array<string>, vfs: Folder): Record<string, Array<File>> {
+  const flatVfs = flattenFolder(vfs)
+  const ruleToVfs: Record<string, Array<File>> = {}
+
+  flatVfs.forEach((entity) => {
+    rules.forEach((rule) => {
+      if (!ruleToVfs[rule]) {
+        ruleToVfs[rule] = []
+      }
+
+      ruleToVfs[rule].push(copyFsEntity(entity))
+    })
+  })
+
+  return ruleToVfs
+}
