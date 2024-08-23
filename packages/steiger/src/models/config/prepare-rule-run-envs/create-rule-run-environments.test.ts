51c070579855748e3df4e7e2f7caa429e4ce8d01
Daniil Sapa <sapadaniil@gmail.com>
2024-08-23 14:47:03 +0300

    Fix tests for createRuleRunEnvironments

--- packages/steiger/src/models/config/prepare-rule-run-envs/create-rule-run-environments.test.ts
+++ packages/steiger/src/models/config/prepare-rule-run-envs/create-rule-run-environments.test.ts
@@ -2,6 +2,7 @@ import { describe, expect, it } from 'vitest'
 import createRuleRunEnvironments from './create-rule-run-environments'
 import { SeverityMarkedFile } from '../types'
 import { Folder } from '@steiger/types'
+import { joinFromRoot } from '../../../_lib/prepare-test'
 
 describe('createRuleRunEnvironments', () => {
   it('should create rule run environments', () => {
@@ -9,44 +10,44 @@ describe('createRuleRunEnvironments', () => {
       rule1: [
         {
           type: 'file',
-          path: '/src/shared/ui/index.ts',
+          path: joinFromRoot('src', 'shared', 'ui', 'index.ts'),
           severity: 'warn',
         },
         {
           type: 'file',
-          path: '/src/shared/ui/Button.tsx',
+          path: joinFromRoot('src', 'shared', 'ui', 'Button.tsx'),
           severity: 'warn',
         },
         {
           type: 'file',
-          path: '/src/entities/user/ui/UserAvatar.tsx',
+          path: joinFromRoot('src', 'entities', 'user', 'ui', 'UserAvatar.tsx'),
           severity: 'off',
         },
         {
           type: 'file',
-          path: '/src/entities/user/ui/index.ts',
+          path: joinFromRoot('src', 'entities', 'user', 'ui', 'index.ts'),
           severity: 'off',
         },
       ],
       rule2: [
         {
           type: 'file',
-          path: '/src/shared/ui/index.ts',
+          path: joinFromRoot('src', 'shared', 'ui', 'index.ts'),
           severity: 'error',
         },
         {
           type: 'file',
-          path: '/src/shared/ui/Button.tsx',
+          path: joinFromRoot('src', 'shared', 'ui', 'Button.tsx'),
           severity: 'error',
         },
         {
           type: 'file',
-          path: '/src/entities/user/ui/UserAvatar.tsx',
+          path: joinFromRoot('src', 'entities', 'user', 'ui', 'UserAvatar.tsx'),
           severity: 'error',
         },
         {
           type: 'file',
-          path: '/src/entities/user/ui/index.ts',
+          path: joinFromRoot('src', 'entities', 'user', 'ui', 'index.ts'),
           severity: 'error',
         },
       ],
@@ -76,30 +77,30 @@ describe('createRuleRunEnvironments', () => {
     expect(ruleRunEnvironments).toEqual({
       rule1: {
         severityMap: {
-          '/src/shared/ui/index.ts': 'warn',
-          '/src/shared/ui/Button.tsx': 'warn',
-          '/src/entities/user/ui/UserAvatar.tsx': 'off',
-          '/src/entities/user/ui/index.ts': 'off',
+          [joinFromRoot('src', 'shared', 'ui', 'index.ts')]: 'warn',
+          [joinFromRoot('src', 'shared', 'ui', 'Button.tsx')]: 'warn',
+          [joinFromRoot('src', 'entities', 'user', 'ui', 'UserAvatar.tsx')]: 'off',
+          [joinFromRoot('src', 'entities', 'user', 'ui', 'index.ts')]: 'off',
         },
         vfs: {
           type: 'folder',
-          path: '/src',
+          path: joinFromRoot('src'),
           children: [
             {
               type: 'folder',
-              path: '/src/shared',
+              path: joinFromRoot('src', 'shared'),
               children: [
                 {
                   type: 'folder',
-                  path: '/src/shared/ui',
+                  path: joinFromRoot('src', 'shared', 'ui'),
                   children: [
                     {
                       type: 'file',
-                      path: '/src/shared/ui/index.ts',
+                      path: joinFromRoot('src', 'shared', 'ui', 'index.ts'),
                     },
                     {
                       type: 'file',
-                      path: '/src/shared/ui/Button.tsx',
+                      path: joinFromRoot('src', 'shared', 'ui', 'Button.tsx'),
                     },
                   ],
                 },
@@ -111,30 +112,30 @@ describe('createRuleRunEnvironments', () => {
       },
       rule2: {
         severityMap: {
-          '/src/shared/ui/index.ts': 'error',
-          '/src/shared/ui/Button.tsx': 'error',
-          '/src/entities/user/ui/UserAvatar.tsx': 'error',
-          '/src/entities/user/ui/index.ts': 'error',
+          [joinFromRoot('src', 'shared', 'ui', 'index.ts')]: 'error',
+          [joinFromRoot('src', 'shared', 'ui', 'Button.tsx')]: 'error',
+          [joinFromRoot('src', 'entities', 'user', 'ui', 'UserAvatar.tsx')]: 'error',
+          [joinFromRoot('src', 'entities', 'user', 'ui', 'index.ts')]: 'error',
         },
         vfs: {
           type: 'folder',
-          path: '/src',
+          path: joinFromRoot('src'),
           children: [
             {
               type: 'folder',
-              path: '/src/shared',
+              path: joinFromRoot('src', 'shared'),
               children: [
                 {
                   type: 'folder',
-                  path: '/src/shared/ui',
+                  path: joinFromRoot('src', 'shared', 'ui'),
                   children: [
                     {
                       type: 'file',
-                      path: '/src/shared/ui/index.ts',
+                      path: joinFromRoot('src', 'shared', 'ui', 'index.ts'),
                     },
                     {
                       type: 'file',
-                      path: '/src/shared/ui/Button.tsx',
+                      path: joinFromRoot('src', 'shared', 'ui', 'Button.tsx'),
                     },
                   ],
                 },
@@ -142,23 +143,23 @@ describe('createRuleRunEnvironments', () => {
             },
             {
               type: 'folder',
-              path: '/src/entities',
+              path: joinFromRoot('src', 'entities'),
               children: [
                 {
                   type: 'folder',
-                  path: '/src/entities/user',
+                  path: joinFromRoot('src', 'entities', 'user'),
                   children: [
                     {
                       type: 'folder',
-                      path: '/src/entities/user/ui',
+                      path: joinFromRoot('src', 'entities', 'user', 'ui'),
                       children: [
                         {
                           type: 'file',
-                          path: '/src/entities/user/ui/UserAvatar.tsx',
+                          path: joinFromRoot('src', 'entities', 'user', 'ui', 'UserAvatar.tsx'),
                         },
                         {
                           type: 'file',
-                          path: '/src/entities/user/ui/index.ts',
+                          path: joinFromRoot('src', 'entities', 'user', 'ui', 'index.ts'),
                         },
                       ],
                     },
@@ -180,22 +181,22 @@ describe('createRuleRunEnvironments', () => {
       rule1: [
         {
           type: 'file',
-          path: '/src/shared/ui/index.ts',
+          path: joinFromRoot('src', 'shared', 'ui', 'index.ts'),
           severity: 'off',
         },
         {
           type: 'file',
-          path: '/src/shared/ui/Button.tsx',
+          path: joinFromRoot('src', 'shared', 'ui', 'Button.tsx'),
           severity: 'off',
         },
         {
           type: 'file',
-          path: '/src/entities/user/ui/UserAvatar.tsx',
+          path: joinFromRoot('src', 'entities', 'user', 'ui', 'UserAvatar.tsx'),
           severity: 'off',
         },
         {
           type: 'file',
-          path: '/src/entities/user/ui/index.ts',
+          path: joinFromRoot('src', 'entities', 'user', 'ui', 'index.ts'),
           severity: 'off',
         },
       ],
@@ -219,10 +220,10 @@ describe('createRuleRunEnvironments', () => {
     expect(ruleRunEnvironments).toEqual({
       rule1: {
         severityMap: {
-          '/src/shared/ui/index.ts': 'off',
-          '/src/shared/ui/Button.tsx': 'off',
-          '/src/entities/user/ui/UserAvatar.tsx': 'off',
-          '/src/entities/user/ui/index.ts': 'off',
+          [joinFromRoot('src', 'shared', 'ui', 'index.ts')]: 'off',
+          [joinFromRoot('src', 'shared', 'ui', 'Button.tsx')]: 'off',
+          [joinFromRoot('src', 'entities', 'user', 'ui', 'UserAvatar.tsx')]: 'off',
+          [joinFromRoot('src', 'entities', 'user', 'ui', 'index.ts')]: 'off',
         },
         vfs: null,
         ruleOptions: {},
