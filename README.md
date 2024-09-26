7057543705cc0636e9a5092e75f597b8876508df
Artur Khantsevich <40607968+Espate@users.noreply.github.com>
2024-09-26 23:17:03 +0200

    Add the `typo-in-layer-name` rule (#102)

--- README.md
+++ README.md
@@ -74,6 +74,7 @@ Currently, Steiger is not extendable with more rules, though that will change in
   <tr> <td><a href="./packages/steiger-plugin-fsd/src/repetitive-naming/README.md"><code>repetitive-naming</code></a></td> <td>Ensure that all entities are named consistently in terms of pluralization.</td> </tr>
   <tr> <td><a href="./packages/steiger-plugin-fsd/src/segments-by-purpose/README.md"><code>segments-by-purpose</code></a></td> <td>Discourage the use of segment names that group code by its essence, and instead encourage grouping by purpose</td> </tr>
   <tr> <td><a href="./packages/steiger-plugin-fsd/src/shared-lib-grouping/README.md"><code>shared-lib-grouping</code></a></td> <td>Forbid having too many ungrouped modules in <code>shared/lib</code>.</td> </tr>
+  <tr> <td><a href="./packages/steiger-plugin-fsd/src/typo-in-layer-name/README.md"><code>typo-in-layer-name</code></a></td> <td>Ensure that all layers are named without any typos.</td> </tr>
   <tr> <td><a href="./packages/steiger-plugin-fsd/src/no-processes/README.md"><code>no-processes</code></a></td> <td>Discourage the use of the deprecated Processes layer.</td> </tr>
   <tr> <td><a href="./packages/steiger-plugin-fsd/src/import-locality/README.md"><code>import-locality</code></a></td> <td>Require that imports from the same slice be relative and imports from one slice to another be absolute.</td> </tr>
 </tbody>
---
+++ packages/steiger-plugin-fsd/src/typo-in-layer-name/index.spec.ts
@@ -0,0 +1,100 @@
+import { expect, it } from 'vitest'
+
+import typoInLayerName from './index.js'
+import { joinFromRoot, parseIntoFsdRoot } from '../_lib/prepare-test.js'
+
+it('reports no errors on a project without typos in layer names', () => {
+  const root = parseIntoFsdRoot(`
+    📂 shared
+    📂 entities
+    📂 features
+    📂 widgets
+    📂 pages
+    📂 app
+  `)
+
+  expect(typoInLayerName.check(root)).toEqual({ diagnostics: [] })
+})
+
+it('reports errors on a project with typos in layer names', () => {
+  const root = parseIntoFsdRoot(`
+    📂 shraed
+    📂 entities
+    📂 fietures
+    📂 wigdets
+    📂 page
+    📂 app
+  `)
+
+  const diagnostics = typoInLayerName.check(root).diagnostics
+  expect(diagnostics).toEqual([
+    {
+      message: 'Layer "page" potentially contains a typo. Did you mean "pages"?',
+      location: { path: joinFromRoot('page') },
+    },
+    {
+      message: 'Layer "shraed" potentially contains a typo. Did you mean "shared"?',
+      location: { path: joinFromRoot('shraed') },
+    },
+    {
+      message: 'Layer "fietures" potentially contains a typo. Did you mean "features"?',
+      location: { path: joinFromRoot('fietures') },
+    },
+    {
+      message: 'Layer "wigdets" potentially contains a typo. Did you mean "widgets"?',
+      location: { path: joinFromRoot('wigdets') },
+    },
+  ])
+})
+
+it('reports no errors on a project with custom layers if base layers are present', () => {
+  const root = parseIntoFsdRoot(`
+    📂 shared
+    📂 shapes
+    📂 entities
+    📂 entries
+    📂 features
+    📂 fixtures
+    📂 widgets
+    📂 pages
+    📂 places
+    📂 app
+    📂 amp
+  `)
+
+  expect(typoInLayerName.check(root)).toEqual({ diagnostics: [] })
+})
+
+it('reports errors on a project with custom layers if base layers are absent', () => {
+  const root = parseIntoFsdRoot(`
+    📂 shapes
+    📂 entries
+    📂 fixtures
+    📂 places
+    📂 amp
+  `)
+
+  const diagnostics = typoInLayerName.check(root).diagnostics
+  expect(diagnostics).toEqual([
+    {
+      message: 'Layer "amp" potentially contains a typo. Did you mean "app"?',
+      location: { path: joinFromRoot('amp') },
+    },
+    {
+      message: 'Layer "shapes" potentially contains a typo. Did you mean "shared"?',
+      location: { path: joinFromRoot('shapes') },
+    },
+    {
+      message: 'Layer "entries" potentially contains a typo. Did you mean "entities"?',
+      location: { path: joinFromRoot('entries') },
+    },
+    {
+      message: 'Layer "fixtures" potentially contains a typo. Did you mean "features"?',
+      location: { path: joinFromRoot('fixtures') },
+    },
+    {
+      message: 'Layer "places" potentially contains a typo. Did you mean "pages"?',
+      location: { path: joinFromRoot('places') },
+    },
+  ])
+})
