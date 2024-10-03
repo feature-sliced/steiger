ff580ee458aac83ca18567cac70871a9b5d0b898
Daniil Sapa <sapadaniil@gmail.com>
2024-10-03 19:04:12 +0300

    Remove vfs-severity-map because it is no longer needed

--- packages/steiger/src/models/vfs-severity-map/types.ts
+++
@@ -1,9 +0,0 @@
-import { File, Folder, Severity } from '@steiger/types'
-
-export interface SeverityMarkedFile extends File {
-  severity: Severity
-}
-
-export interface SeverityMarkedFolder extends Folder {
-  children: Array<SeverityMarkedFolder | SeverityMarkedFile>
-}
