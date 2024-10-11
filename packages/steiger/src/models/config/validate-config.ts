cd9bd55d49b1a6f8b3c77fda5ad3047f127e8d96
Daniil Sapa <sapadaniil@gmail.com>
2024-10-12 00:36:04 +0300

    Add error messages for incorrect config shapes

--- packages/steiger/src/models/config/validate-config.ts
+++ packages/steiger/src/models/config/validate-config.ts
@@ -1,8 +1,16 @@
 import z from 'zod'
 
+import { BaseRuleOptions, Config, Plugin, Severity } from '@steiger/types'
+
 import { getOptions, isConfigObject, isPlugin } from './raw-config'
 import { isEqual } from '../../shared/objects'
-import { BaseRuleOptions, Config, Plugin, Severity } from '@steiger/types'
+
+const OLD_CONFIG_ERROR_MESSAGE =
+  'Old configuration structure noticed. We are evolving!\nPlease follow these easy 5-minute instructions to migrate to the new one:\nhttps://github.com/feature-sliced/steiger/blob/master/MIGRATION_GUIDE.md'
+const WRONG_CONFIG_SHAPE_ERROR_MESSAGE =
+  'The provided config has a wrong shape.\nPlease check your configuration file and follow the documentation to fix it:\nhttps://github.com/feature-sliced/steiger?tab=readme-ov-file#configuration'
+const NO_RULES_ERROR_MESSAGE = 'At least one rule must be provided by plugins!'
+const NO_CONFIG_OBJECTS_ERROR_MESSAGE = 'At least one config object must be provided!'
 
 function getAllRuleNames(plugins: Array<Plugin>) {
   const allRules = plugins.flatMap((plugin) => plugin.ruleDefinitions)
@@ -15,7 +23,7 @@ function validateConfigObjectsNumber(value: Config, ctx: z.RefinementCtx) {
   if (configObjects.length === 0) {
     ctx.addIssue({
       code: z.ZodIssueCode.custom,
-      message: 'At least one config object must be provided!',
+      message: NO_CONFIG_OBJECTS_ERROR_MESSAGE,
     })
   }
 }
@@ -69,13 +77,13 @@ function validateRuleOptions(value: Config, ctx: z.RefinementCtx) {
 /**
  * Dynamically build a validation scheme based on the rules provided by plugins.
  * */
-export default function buildValidationScheme(rawConfig: Config) {
+export function buildValidationScheme(rawConfig: Config) {
   const allRuleNames = getAllRuleNames(rawConfig.filter(isPlugin))
 
   // Make sure there's at least one rule registered by plugins
   // Need to check this before creating the scheme, because zod.enum requires at least one element
   if (allRuleNames.length === 0) {
-    throw new Error('At least one rule must be provided by plugins!')
+    throw new Error(NO_RULES_ERROR_MESSAGE)
   }
 
   // Marked as "any" because return type is not useful for this validation
@@ -123,4 +131,22 @@ export default function buildValidationScheme(rawConfig: Config) {
     .superRefine(validateConfigObjectsNumber)
     .superRefine(validateRuleOptions)
     .superRefine(validateRuleUniqueness)
+}
+
+export function validateConfig(rawConfig: Config) {
+  const isOldConfig = typeof rawConfig === 'object' && !Array.isArray(rawConfig)
+  const isWrongShape = !Array.isArray(rawConfig)
+
+  // Need to check the shape of the config separately before validating it,
+  // because building a validation scheme requires the config to be an array
+  if (isOldConfig) {
+    throw new Error(OLD_CONFIG_ERROR_MESSAGE)
+  }
+
+  if (isWrongShape) {
+    throw new Error(WRONG_CONFIG_SHAPE_ERROR_MESSAGE)
+  }
+
+  const validationScheme = buildValidationScheme(rawConfig)
+  return validationScheme.parse(rawConfig)
 }
