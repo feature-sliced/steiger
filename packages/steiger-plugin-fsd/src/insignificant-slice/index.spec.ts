5294adcb86801cb9649d04a30572ee30fb43374c
Daniil Sapa <sapadaniil@gmail.com>
2024-10-16 01:53:11 +0300

    Add a test case for vue file structures for insignificant-slice rule

--- packages/steiger-plugin-fsd/src/insignificant-slice/index.spec.ts
+++ packages/steiger-plugin-fsd/src/insignificant-slice/index.spec.ts
@@ -21,14 +21,25 @@ vi.mock('node:fs', async (importOriginal) => {
       '/shared/ui/Button.tsx': 'import styles from "./styles";',
       '/shared/ui/TextField.tsx': 'import styles from "./styles";',
       '/shared/ui/index.ts': '',
+
       '/entities/user/@x/product.ts': '',
       '/entities/user/ui/UserAvatar.tsx': 'import { Button } from "@/shared/ui"',
       '/entities/user/index.ts': '',
       '/entities/product/ui/ProductCard.tsx': '',
       '/entities/product/ui/CrossReferenceCard.tsx': 'import { UserAvatar } from "@/entities/user/@x/product"',
       '/entities/product/index.ts': '',
+      '/entities/post/index.ts': '',
+      '/entities/post/ui/post-card.vue': '',
+
       '/features/comments/ui/CommentCard.tsx': '',
       '/features/comments/index.ts': '',
+      '/features/session/register/index.ts': 'import {RegisterForm} from "./ui/RegisterForm.vue"',
+      '/features/session/register/ui/RegisterForm.vue': '',
+      '/features/session/logout/index.ts': 'import {LogoutButton} from "./ui/LogoutButton.vue"',
+      '/features/session/logout/ui/LogoutButton.vue': '',
+      '/features/session/login/index.ts': 'import {LoginForm} from "./ui/LoginForm.vue"',
+      '/features/session/login/ui/LoginForm.vue': '',
+
       '/pages/editor/ui/EditorPage.tsx':
         'import { Button } from "@/shared/ui"; import { Editor } from "./Editor"; import { CommentCard } from "@/features/comments"; import { UserAvatar } from "@/entities/user"',
       '/pages/editor/ui/Editor.tsx':
@@ -37,6 +48,12 @@ vi.mock('node:fs', async (importOriginal) => {
       '/pages/settings/ui/SettingsPage.tsx':
         'import { Button } from "@/shared/ui"; import { CommentCard } from "@/features/comments"',
       '/pages/settings/index.ts': '',
+      '/pages/home/index.ts': '',
+      '/pages/home/ui/home.vue':
+        '<template>Home page</template> <script>import { PostCard } from "@/entities/post"; import {RegisterFrom} from "@/features/session/register"; import {LoginForm} from "@/features/session/login"; import {LogoutButton} from "@/features/session/logout"</script>',
+      '/pages/category/index.ts': '',
+      '/pages/category/ui/category.vue':
+        '<template>Home page</template> <script>import { PostCard } from "@/entities/post"; import {RegisterFrom} from "@/features/session/register"; import {LoginForm} from "@/features/session/login"; import {LogoutButton} from "@/features/session/logout"</script>',
     },
     originalFs,
   )
@@ -166,4 +183,39 @@ it('reports errors on a project where the only other reference to a slice is a c
       location: { path: joinFromRoot('entities', 'user') },
     },
   ])
+})
+
+it('should report no errors on a project with Vue Single-File Components (*.vue files)', async () => {
+  const root = parseIntoFsdRoot(`
+    📂 entities
+      📂 post
+        📂 ui
+          📄 post-card.vue
+        📄 index.ts
+    📂 features
+      📂 session
+        📂 login
+          📂 ui
+            LoginForm.vue
+          📄 index.ts
+        📂 logout
+          📂 ui
+            LogoutButton.vue
+          📄 index.ts
+        📂 register
+          📂 ui
+            RegisterForm.vue
+          📄 index.ts
+    📂 pages
+      📂 home
+        📂 ui
+          📄 home.vue
+        📄 index.ts
+      📂 category
+        📂 ui
+          📄 category.vue
+        📄 index.ts
+  `)
+
+  expect((await insignificantSlice.check(root)).diagnostics).toEqual([])
 })
