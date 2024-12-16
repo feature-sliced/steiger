81817ff95dc46648ada1395d723df2b086275896
Daniil Sapa <sapadaniil@gmail.com>
2024-12-16 15:33:25 +0200

    Replace chalk with pinocolors, make the diagnostic location more readable

--- packages/pretty-reporter/src/format-single-diagnostic.ts
+++ packages/pretty-reporter/src/format-single-diagnostic.ts
@@ -1,18 +1,19 @@
 import { relative } from 'node:path'
 import figures from 'figures'
 import terminalLink from 'terminal-link'
-import chalk from 'chalk'
+import picocolors from 'picocolors'
 import type { Diagnostic } from '@steiger/types'
 
 export function formatSingleDiagnostic(d: Diagnostic, cwd: string): string {
-  const x = d.severity === 'error' ? chalk.red(figures.cross) : chalk.yellow(figures.warning)
-  const s = chalk.reset(figures.lineDownRight)
-  const bar = chalk.reset(figures.lineVertical)
-  const e = chalk.reset(figures.lineUpRight)
-  const message = chalk.reset(d.message)
-  const autofixable = d.fixes !== undefined && d.fixes.length > 0 ? chalk.green(`${figures.tick} Auto-fixable`) : null
-  const location = chalk.gray(formatLocation(d.location, cwd))
-  const ruleName = chalk.blue(terminalLink(d.ruleName, d.getRuleDescriptionUrl(d.ruleName).toString()))
+  const x = d.severity === 'error' ? picocolors.red(figures.cross) : picocolors.yellow(figures.warning)
+  const s = picocolors.reset(figures.lineDownRight)
+  const bar = picocolors.reset(figures.lineVertical)
+  const e = picocolors.reset(figures.lineUpRight)
+  const message = picocolors.reset(d.message)
+  const autofixable =
+    d.fixes !== undefined && d.fixes.length > 0 ? picocolors.green(`${figures.tick} Auto-fixable`) : null
+  const location = picocolors.dim(formatLocation(d.location, cwd))
+  const ruleName = picocolors.blue(terminalLink(d.ruleName, d.getRuleDescriptionUrl(d.ruleName).toString()))
 
   return `
 ${s} ${location}
