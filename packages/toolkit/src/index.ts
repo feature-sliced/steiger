9300bf0faccda7e66f3738d0cf9b8a83822f728f
Gunwoo Baik <gwbaik9717@icloud.com>
2025-03-29 08:59:36 +0000

    Enable Specific Rules(Disable some rules for default)

--- packages/toolkit/src/index.ts
+++ packages/toolkit/src/index.ts
@@ -1,6 +1,6 @@
 export type * from '@steiger/types'
 
 export { findAllRecursively } from './find-all-recursively.js'
-export { enableAllRules, createConfigs } from './create-configs.js'
+export { enableAllRules, createConfigs, enableSpecificRules } from './create-configs.js'
 export { createPlugin } from './create-plugin.js'
 export type { ConfigObjectOf } from './config-object-of.js'
