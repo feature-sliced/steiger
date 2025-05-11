89a5b3919e86bb2a02e778f5a63377cdc2edece4
Lev Chelyadinov <leva181777@gmail.com>
2025-05-11 15:29:07 +0200

    Install the Steiger plugin through a tarball during testing

--- integration-tests/tests/smoke.test.ts
+++ integration-tests/tests/smoke.test.ts
@@ -8,8 +8,10 @@ import { expect, test } from 'vitest'
 
 import { getSteigerBinPath } from '../utils/get-bin-path.js'
 import { getSnapshotPath } from '../utils/get-snapshot-path.js'
+import { getRepoRootPath } from '../utils/get-repo-root-path.js'
 
 const temporaryDirectory = await fs.realpath(os.tmpdir())
+const repoRoot = getRepoRootPath()
 const steiger = await getSteigerBinPath()
 const kitchenSinkExample = join(dirname(fileURLToPath(import.meta.url)), '../../examples/kitchen-sink-of-fsd-issues')
 
@@ -18,7 +20,17 @@ test('basic functionality in the kitchen sink example project', async () => {
   await fs.rm(project, { recursive: true, force: true })
   await fs.cp(kitchenSinkExample, project, { recursive: true })
 
+  const steigerPluginPath = join(repoRoot, 'packages', 'steiger-plugin-fsd')
+  const { stdout: steigerPluginTarball } = await exec('npm', ['pack'], {
+    nodeOptions: { cwd: steigerPluginPath },
+  })
+  await exec('npm', ['install', join(steigerPluginPath, steigerPluginTarball.trim())], {
+    nodeOptions: { cwd: project },
+  })
+
   const { stderr } = await exec('node', [steiger, 'src'], { nodeOptions: { cwd: project, env: { NO_COLOR: '1' } } })
 
   await expect(stderr).toMatchFileSnapshot(getSnapshotPath('smoke-stderr'))
+
+  await fs.rm(join(steigerPluginPath, steigerPluginTarball.trim()))
 })
