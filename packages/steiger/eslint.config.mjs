ef2131f570e7e93a3de0742cedd8e1672e42ec3f
Solant <runner62v6@gmail.com>
2026-05-25 00:55:35 +0200

    ci: add eslint-plugin-package-json config

--- packages/steiger/eslint.config.mjs
+++ packages/steiger/eslint.config.mjs
@@ -1,1 +1,4 @@
-export { default } from '@steiger/eslint-config'
+import packageJson from 'eslint-plugin-package-json'
+import baseConfig from '@steiger/eslint-config'
+
+export default [...baseConfig, packageJson.configs['recommended-publishable']]
