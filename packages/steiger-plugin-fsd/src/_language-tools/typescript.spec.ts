d888142bc54925eb3250f4e39dcdc80c0806940e
Solant <runner62v6@gmail.com>
2026-07-07 23:00:10 +0200

    test: add mock-node-fs helper and update tests

--- packages/steiger-plugin-fsd/src/_language-tools/typescript.spec.ts
+++ packages/steiger-plugin-fsd/src/_language-tools/typescript.spec.ts
@@ -1,34 +1,36 @@
-import { expect, it } from 'vitest'
-import { extractDependencies } from './index.js'
+import { expect, it, vi } from 'vitest'
 
-it('extracts esm dependencies from TypeScript source code', async () => {
-  const dependencies = await extractDependencies(
-    'tsx',
-    `
+import { createMockedNodeFs } from './mock-node-fs.js'
+
+vi.mock('node:fs', () =>
+  createMockedNodeFs({
+    '/src/esm.tsx': `
     import isEven from 'is-even'
   `,
-  )
-  expect(dependencies).toEqual(['is-even'])
-})
-
-it('extracts cjs dependencies from TypeScript source code', async () => {
-  const dependencies = await extractDependencies(
-    'tsx',
-    `
+    '/src/cjs.tsx': `
     const isEven = require('is-even')
   `,
-  )
-  expect(dependencies).toEqual(['is-even'])
-})
-
-it('extracts dynamic dependencies from TypeScript source code', async () => {
-  const dependencies = await extractDependencies(
-    'tsx',
-    `
+    '/src/dynamic.tsx': `
     async function foo() {
       const isEven = await import('is-even')
     }
   `,
-  )
+  }),
+)
+
+import { extractDependencies } from './index.js'
+
+it('extracts esm dependencies from TypeScript source code', async () => {
+  const dependencies = await extractDependencies('/src/esm.tsx')
+  expect(dependencies).toEqual(['is-even'])
+})
+
+it('extracts cjs dependencies from TypeScript source code', async () => {
+  const dependencies = await extractDependencies('/src/cjs.tsx')
+  expect(dependencies).toEqual(['is-even'])
+})
+
+it('extracts dynamic dependencies from TypeScript source code', async () => {
+  const dependencies = await extractDependencies('/src/dynamic.tsx')
   expect(dependencies).toEqual(['is-even'])
 })
