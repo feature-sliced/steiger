131f9616d200bc9e7ce36f9c0c120a239c4cae71
Solant <runner62v6@gmail.com>
2026-09-24 11:13:33 +0200

    feat: update nodejs version, fix typescript 7 support

---
+++ .changeset/lazy-cases-wish.md
@@ -0,0 +1,9 @@
+---
+'@steiger/pretty-reporter': minor
+'@steiger/integration-tests': minor
+'steiger': minor
+'@steiger/tsconfig': minor
+'@steiger/types': minor
+---
+
+update minimal nodejs version, fix typescript 7 support
--- packages/steiger/src/features/choose-root-folder/choose-from-similar.ts
+++ packages/steiger/src/features/choose-root-folder/choose-from-similar.ts
@@ -12,7 +12,6 @@ import { ExitException } from './exit-exception'
 /** The maximum Levenshtein distance between the input and the reference for the input to be considered a typo. */
 const typoThreshold = 5
 const gitFolder = find.up('.git')
-// @ts-expect-error suppressErrors is missing from the type definition
 const isIgnored = await isGitIgnored({ cwd: gitFolder ? dirname(gitFolder) : undefined, suppressErrors: true })
 
 /** Present the user with a choice of folders based on similarity to a given input. */
